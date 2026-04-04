42e63d545db78d2cf93613d89b3c0207db41e0c1
Franco Perotti <franco.perotti@mi.unc.edu.ar>
2026-04-04 16:48:42 -0300

    style(main): mejora formato y limpieza de código en manejo de entrada y salida
    - Corrige espacios y uso de comillas para mayor consistencia
    - Simplifica el bucle principal y la lógica de continuación
    - Elimina comentarios y código comentado innecesario

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import requests
 import ctypes
 
-# Cargamos la libreria 
-lib_converter = ctypes.CDLL('./to_int_plus_one.so')
+# Cargamos la libreria
+lib_converter = ctypes.CDLL("./to_int_plus_one.so")
 
 # Definimos los tipos de los argumentos de la función factorial
 lib_converter.to_int_plus_one.argtypes = (ctypes.c_float,)
@@ -10,26 +10,26 @@ lib_converter.to_int_plus_one.argtypes = (ctypes.c_float,)
 # Definimos el tipo del retorno de la función factorial
 lib_converter.to_int_plus_one.restype = ctypes.c_int
 
+
 # Creamos nuestra función factorial en Python
 # hace de Wrapper para llamar a la función de C
 def float_to_int_plus_one(num):
-    return  lib_converter.to_int_plus_one(num)
+    return lib_converter.to_int_plus_one(num)
 
 
 flag = True
 
-print("gini>> Este es un programa para obtener los valores, de un cierto pais, del indice GINI\n")
+print(
+    "gini>> Este es un programa para obtener los valores, de un cierto pais, del indice GINI\n"
+)
 
-while(flag):
+while flag:
     country = input("gini>> Ingrese un pais: ")
 
     URL = f"https://api.worldbank.org/v2/country/{country}/indicator/SI.POV.GINI?format=json&date=2011:2020&per_page=1000"
 
     response = requests.get(URL)
     values = response.json()[1]
-
-    #for data in values:
-    #    print(f"{data}\n")
 
     for item in values:
         if item["value"] == None:
@@ -38,9 +38,7 @@ while(flag):
         int_converted = float_to_int_plus_one(item["value"])
         print(f"date: {item['date']}, value: {int_converted}\n")
 
-    flag = input("Continuar --> 1\nFinalizar --> 0\n")
-    if flag == False:
-        break
+    flag = input("Continuar --> 1\nFinalizar --> 0\n").strip() == "1"
 
 
 print("Programa finalizado.")
